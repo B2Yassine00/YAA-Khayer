a8d2e030e8941507f96e21e2815b553451be775b
B2Yassine00 <banouq90@gmail.com>
2023-05-24 13:52:37 +0100

    test the authentication

---
+++ BackEnd-no-context/yaa-khayer/src/test/java/com/project/yaakhayer/controller/AuthenticationControllerTest.java
@@ -0,0 +1,16 @@
+package com.project.yaakhayer.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationControllerTest {
+
+    @Test
+    void signin() {
+    }
+
+    @Test
+    void authenticate() {
+    }
+}
